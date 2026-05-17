cc1d3d310e5a77458f01d38fd374f0eab50175cb
khushiarzare <khushiarzare525@gmail.com>
2026-05-17 09:29:53 +0530

    feat: enhance landing page UI with improved styling and layout adjustments

--- frontend/src/components/landing.jsx
+++ frontend/src/components/landing.jsx
@@ -4,17 +4,17 @@ export default function Landing() {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center px-4">
-      <div className="bg-gray-100 border border-white border-bold rounded-2xl p-10 w-full max-w-md text-center">
+    <div className="min-h-screen bg-linear-to-r from-red-950  to-black flex items-center justify-center px-4">
+      <div className="bg-black-100  border-white border-4 rounded-2xl p-10 w-full max-w-md text-center">
 
         {/* Logo */}
-        <div className="w-14 h-14 rounded-full bg-blue-100 text-blue-600 font-medium text-lg flex items-center justify-center mx-auto mb-4">
+        <div className="w-16 h-16 rounded-full bg-white border-red-900 border-2 text-red-950 font-medium text-lg flex items-center justify-center mx-auto mb-4">
           HR
         </div>
 
         {/* Title */}
-        <h1 className="text-2xl font-medium text-gray-900 mb-2">AI Resume Analyzer</h1>
-        <p className="text-sm text-gray-500 mb-8">
+        <h1 className="text-3xl font-medium text-white mb-2">AI Resume Analyzer</h1>
+        <p className="text-sm text-gray-400 mb-8">
           Analyze resumes smartly. Find the best candidates faster.
         </p>
 
@@ -22,13 +22,13 @@ export default function Landing() {
         <div className="flex flex-col gap-3">
           <button
             onClick={() => navigate("/login")}
-            className="w-full py-2.5 bg-white text-gray-900 text-sm font-medium rounded-lg  border border-gray-200 hover:bg-gray-600 transition-colors hover:scale-105 duration-200  cursor-pointer"
+            className="w-full py-2.5 bg-white text-gray-900 text-l font-medium rounded-lg  border border-gray-200 hover:border-whitehover:text-white transition-colors hover:scale-105 duration-200  cursor-pointer"
           >
             Login
           </button>
           <button
             onClick={() => navigate("/signup")}
-            className="w-full py-2.5 bg-white text-gray-900 text-sm font-medium rounded-lg border border-gray-200 hover:bg-gray-600 transition-colors hover:scale-105 duration-200  cursor-pointer"
+            className="w-full py-2.5 bg-white text-gray-900 text-l font-medium rounded-lg border border-gray-200 hover:border-white  hover:bg-red-950  hover:text-white  transition-colors hover:scale-105 duration-200  cursor-pointer"
           >
             Sign Up
           </button>
